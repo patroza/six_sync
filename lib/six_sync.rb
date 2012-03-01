cf583cac0bd99d0564ce4b4bfce781ccb98edc84
Sickboy <sb@dev-heaven.net>
2012-03-01 19:52:30 +0100

    Added header

--- lib/six_sync.rb
+++ lib/six_sync.rb
@@ -1,3 +1,15 @@
+=begin
+  Copyright:   Copyright (C) 2011-2012 Patrick Roza. All rights reserved.
+  Website:     http://www.six-updater.net
+  Author:      Patrick Roza
+  E-mail:      sb@dev-heaven.net
+
+  The copyright to the source code and computer program(s) herein is the property of Patrick Roza, The Netherlands.
+  The source code and program(s) may be used and/or copied only with the written permission of Patrick Roza or in
+  accordance with the terms and conditions stipulated in the agreement/contract under which the source code and
+  program(s) have been supplied.
+=end
+
 require "six_sync/version"
 
 module SixSync
