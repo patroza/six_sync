c7493c0ac1172e5bafe720892f56943e313c7450
Sickboy <sb@dev-heaven.net>
2012-03-02 18:45:53 +0100

    sync_directory is now an optional parameter, and defaults to current_working_directory/DEFAULT_SYNC_DIRECTORY_NAME

--- lib/six_sync/repository.rb
+++ lib/six_sync/repository.rb
@@ -3,8 +3,11 @@ module SixSync
   # Repositories represent the combination of all components,
   # It's the master class that controls the others to perform actions on the repository
   class Repository
+    # Default folder name of repository root
+    DEFAULT_SYNC_DIRECTORY_NAME = ".sync"
+
     # Default folder name of archived content
-    DEFAULT_PACK_NAME = ".pack"
+    DEFAULT_PACK_DIRECTORY_NAME = ".pack"
 
     # Represents the status of the object
     attr_reader :status
@@ -23,13 +26,13 @@ module SixSync
     #
     # === Attributes
     #
-    # +sync_directory+ - repository directory with configuration files
-    # +working_directory+ - working directory with unarchived root
-    # +pack_directory+ - working directory with archived root
-    def initialize sync_directory, working_directory = nil, pack_directory = nil
-      @sync_directory = sync_directory
+    # * +sync_directory+ - repository directory with configuration files, defaults to current_working_directory/{DEFAULT_SYNC_DIRECTORY_NAME}
+    # * +working_directory+ - working directory with unarchived root, defaults to parent of sync_directory
+    # * +pack_directory+ - working directory with archived root, defaults to sync_directory/{DEFAULT_PACK_DIRECTORY_NAME}
+    def initialize sync_directory = nil, working_directory = nil, pack_directory = nil
+      @sync_directory = sync_directory.nil? ?  ::File.join(::Dir.pwd, DEFAULT_SYNC_DIRECTORY_NAME) : sync_directory
       @working_directory = working_directory.nil? ? ::File.dirname(sync_directory) : working_directory
-      @pack_directory = pack_directory.nil? ? ::File.join(sync_directory, DEFAULT_PACK_NAME) : pack_directory
+      @pack_directory = pack_directory.nil? ? ::File.join(sync_directory, DEFAULT_PACK_DIRECTORY_NAME) : pack_directory
 
       @status = :default
     end
