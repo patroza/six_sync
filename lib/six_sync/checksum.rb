f5ecf6fc84144fe67451d6038025e0ce3d033b75
Sickboy <sb@dev-heaven.net>
2012-03-02 15:39:45 +0100

    Added Checksum RDoc

--- lib/six_sync/checksum.rb
+++ lib/six_sync/checksum.rb
@@ -1,27 +1,46 @@
 module SixSync
+  ##
+  # Checksums represent the checksums of Files, usually in MD5 or SHA1 format
   class Checksum
-    attr_accessor :status
+    # Represents the status of the object
+    attr_reader :status
+
+    ##
+    # Initializes the object
+    def initialize
+      @status = :default
+    end
 
     class <<self
+      ##
+      # Contains the registered subclasses checksum types
       def checksum_types; @@checksum_types ||= []; end
+
+      ##
+      # Verifies if the checksum_type is registered
+      # === Attributes
+      #
+      # * +checksum_type+ - The checksum_type to lookup
       def has_checksum_type? checksum_type
         self.checksum_types.include?(SixSync.const_get "#{checksum_type.capitalize}Checksum")
       end
 
+      ##
+      # Handles the registration of the subclasses
       def inherited subclass
         self.checksum_types << subclass
       end
     end
-
-    def status
-      :default
-    end
   end
 
+  ##
+  # Md5 variant of Checksum
   class Md5Checksum < Checksum
 
   end
 
+  ##
+  # Sha1 variant of Checksum
   class Sha1Checksum < Checksum
 
   end
