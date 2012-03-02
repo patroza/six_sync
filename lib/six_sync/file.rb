1f41f8fdc46d4e74c89205f23fada551314fd385
Sickboy <sb@dev-heaven.net>
2012-03-02 15:39:57 +0100

    Added File RDoc

--- lib/six_sync/file.rb
+++ lib/six_sync/file.rb
@@ -1,27 +1,47 @@
 module SixSync
+  ##
+  # Files represent the objects within the repository
+  # Usually consisting of a packed and unpacked equilevant
   class File
-    attr_accessor :status
+    # Represents the status of the object
+    attr_reader :status
+
+    ##
+    # Initializes the object
+    def initialize
+      @status = :default
+    end
 
     class <<self
+      ##
+      # Contains the registered subclasses file types
       def file_types; @@file_types ||= []; end
+
+      ##
+      # Verifies if the file_type is registered
+      # === Attributes
+      #
+      # * +file_type+ - The file_type to lookup
       def has_file_type? file_type
         self.file_types.include?(SixSync.const_get "#{file_type.capitalize}File")
       end
 
+      ##
+      # Handles the registration of the subclasses
       def inherited subclass
         self.file_types << subclass
       end
     end
-
-    def status
-      :default
-    end
   end
 
+  ##
+  # Archived variant of an object
   class PackFile < File
 
   end
 
+  ##
+  # UnArchived variant of an object
   class WdFile < File
 
   end
