99377b418799aca9ac57646ba9000da6708d0573
Sickboy <sb@dev-heaven.net>
2012-03-02 21:20:31 +0100

    Moved options test to Parser context

--- test/units/options_test.rb
+++ test/units/options_test.rb
@@ -8,17 +8,19 @@ module Kernel
 end
 
 context "Options" do
-  setup { SixSync::Options }
+  context "Parser" do
+    setup { SixSync::Options }
 
-  asserts("Parse returns an OpenStruct") { topic.parse.is_a?(OpenStruct) }
-  asserts("Parse tasks is Array") { topic.parse.tasks.is_a?(Array) }
+    asserts("Parse returns an OpenStruct") { topic.parse.is_a?(OpenStruct) }
+    asserts("Parse tasks is Array") { topic.parse.tasks.is_a?(Array) }
 
-  asserts("Parse verbose") { topic.parse(["-v"]).verbose }.equals true
-  asserts("Parse no-verbose") { topic.parse(["--no-verbose"]).verbose }.equals false
+    asserts("Parse verbose") { topic.parse(["-v"]).verbose }.equals true
+    asserts("Parse no-verbose") { topic.parse(["--no-verbose"]).verbose }.equals false
 
-  asserts("Parse version") { topic.parse(["--version"])}.equals 0
-  asserts("Parse help") { topic.parse(["--help"]) }.equals 0
-  asserts("Parse clone") { topic.parse(["--clone", "http://localhost/test"]).tasks }.same_elements [[:clone, "http://localhost/test"]]
-  asserts("Parse init") { topic.parse(["-i"]).tasks }.same_elements [[:init, Dir.pwd]]
-  asserts("Parse init at specified dir") { topic.parse(["--init", "C:/test/test"]).tasks }.same_elements [[:init, "C:/test/test"]]
+    asserts("Parse version") { topic.parse(["--version"])}.equals 0
+    asserts("Parse help") { topic.parse(["--help"]) }.equals 0
+    asserts("Parse clone") { topic.parse(["--clone", "http://localhost/test"]).tasks }.same_elements [[:clone, "http://localhost/test"]]
+    asserts("Parse init") { topic.parse(["-i"]).tasks }.same_elements [[:init, Dir.pwd]]
+    asserts("Parse init at specified dir") { topic.parse(["--init", "C:/test/test"]).tasks }.same_elements [[:init, "C:/test/test"]]
+  end
 end
