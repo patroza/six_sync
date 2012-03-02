d61851185b94ce3ae1a504bf415cf2b6ceab50d4
Sickboy <sb@dev-heaven.net>
2012-03-02 15:09:54 +0100

    Added Checksum class tests

--- lib/six_sync/checksum.rb
+++ lib/six_sync/checksum.rb
@@ -2,6 +2,17 @@ module SixSync
   class Checksum
     attr_accessor :status
 
+    class <<self
+      def checksum_types; @@checksum_types ||= []; end
+      def has_checksum_type? checksum_type
+        self.checksum_types.include?(SixSync.const_get "#{checksum_type.capitalize}Checksum")
+      end
+
+      def inherited subclass
+        self.checksum_types << subclass
+      end
+    end
+
     def status
       :default
     end
--- test/units/checksum_test.rb
+++ test/units/checksum_test.rb
@@ -1,16 +1,31 @@
 require 'teststrap'
 
 context "Checksum" do
+  context "Class" do
+    setup { SixSync::Checksum }
+    asserts("ChecksumTypes is an array") { topic.checksum_types.is_a?(Array) }
+  end
+
   setup { SixSync::Checksum.new }
 
   asserts("Default Status") { topic.status == :default }
 
   context "Md5Checksum" do
+    context "Class" do
+      setup { SixSync::Checksum }
+      asserts("Checksumtype registered") { topic.has_checksum_type?(:md5) }
+    end
+
     setup { SixSync::Md5Checksum.new }
 
     asserts("Default Status") { topic.status == :default }
   end
   context "Sha1Checksum" do
+    context "Class" do
+      setup { SixSync::Checksum }
+      asserts("Checksumtype registered") { topic.has_checksum_type?(:sha1) }
+    end
+
     setup { SixSync::Sha1Checksum.new }
 
     asserts("Default Status") { topic.status == :default }
