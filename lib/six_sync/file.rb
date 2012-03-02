d65be9db77d10b6781a137dcaf4426aecf399f2a
Sickboy <sb@dev-heaven.net>
2012-03-02 15:10:16 +0100

    Added File class tests

--- lib/six_sync/file.rb
+++ lib/six_sync/file.rb
@@ -1,6 +1,18 @@
 module SixSync
   class File
     attr_accessor :status
+
+    class <<self
+      def file_types; @@file_types ||= []; end
+      def has_file_type? file_type
+        self.file_types.include?(SixSync.const_get "#{file_type.capitalize}File")
+      end
+
+      def inherited subclass
+        self.file_types << subclass
+      end
+    end
+
     def status
       :default
     end
--- test/units/file_test.rb
+++ test/units/file_test.rb
@@ -1,16 +1,31 @@
 require 'teststrap'
 
 context "File" do
+  context "Class" do
+    setup { SixSync::File }
+    asserts("FileTypes is an array") { topic.file_types.is_a?(Array) }
+  end
+
   setup { SixSync::File.new }
 
   asserts("Default Status") { topic.status == :default }
 
   context "PackFile" do
+    context "Class" do
+      setup { SixSync::File }
+      asserts("Filetype registered") { topic.has_file_type?(:pack) }
+    end
+
     setup { SixSync::PackFile.new }
     asserts("Default Status") { topic.status == :default }
   end
 
   context "WdFile" do
+    context "Class" do
+      setup { SixSync::File }
+      asserts("Filetype registered") { topic.has_file_type?(:wd) }
+    end
+
     setup { SixSync::WdFile.new }
     asserts("Default Status") { topic.status == :default }
   end
