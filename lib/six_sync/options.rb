6749cb59a60e156ad0a444dd05d80bcf415155e8
Sickboy <sb@dev-heaven.net>
2012-03-03 18:22:00 +0100

    Prepared Clone, Init, Update, Repair, Commit, Push options for Network Options

--- lib/six_sync/options.rb
+++ lib/six_sync/options.rb
@@ -90,7 +90,65 @@ module SixSync
       private
       def _parse options
         OptionParser.new do |opts|
+          opts.banner = "Usage: #{$0} [options]"
+          opts.separator ""
+          opts.separator "Specific options:"
 
+          opts.on("-c", "--clone URL",
+                  "Clone from given URL") do |url|
+            options.tasks << [:clone, url]
+          end
+
+          opts.on("-i", "--init [DIR]",
+                  "Init at given dir, or current dir if unspecified") do |dir|
+            dir = Dir.pwd if dir.nil?
+            options.tasks << [:init, dir]
+          end
+
+          opts.on("-u", "--update [DIR]",
+                  "Update the given dir, or current dir if unspecified") do |dir|
+            dir = Dir.pwd if dir.nil?
+            options.tasks << [:update, dir]
+          end
+
+          opts.on("-r", "--repair [DIR]",
+                  "Repair the given dir, or current dir if unspecified") do |dir|
+            dir = Dir.pwd if dir.nil?
+            options.tasks << [:repair, dir]
+          end
+
+          opts.on("-c", "--commit [DIR]",
+                  "Commit the given dir, or current dir if unspecified") do |dir|
+            dir = Dir.pwd if dir.nil?
+            options.tasks << [:commit, dir]
+          end
+
+          opts.on("-p", "--push [DIR]",
+                  "Push the given dir, or current dir if unspecified") do |dir|
+            dir = Dir.pwd if dir.nil?
+            options.tasks << [:push, dir]
+          end
+
+          # Boolean switch.
+          opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+            options.verbose = v
+          end
+
+          opts.separator ""
+          opts.separator "Common options:"
+
+          # No argument, shows at tail.  This will print an options summary.
+          # Try it and see!
+          opts.on_tail("-h", "--help", "Show this message") do
+            puts opts
+            exit
+          end
+
+          # Another typical switch to print the version.
+          opts.on_tail("-v", "--version", "Show version") do
+            puts SixSync::VERSION
+            exit
+          end
         end
       end
     end
--- test/units/options_test.rb
+++ test/units/options_test.rb
@@ -16,9 +16,9 @@ context "Options" do
       asserts("version") { mock(SixSync::Options).exit { 0 }; topic.parse(["--version"])} #.equals 0
       asserts("help") { mock(SixSync::Options).exit { 0 }; topic.parse(["--help"]) } #.equals 0
 
-      asserts("clone") { topic.parse(["--clone", "http://localhost/test"]).tasks }.same_elements [[:clone, "http://localhost/test"]]
       asserts("init") { topic.parse(["--init"]).tasks }.same_elements [[:init, Dir.pwd]]
       asserts("init at specified dir") { topic.parse(["--init", "C:/test/test"]).tasks }.same_elements [[:init, "C:/test/test"]]
+      asserts("clone") { topic.parse(["--clone", "http://localhost/test"]).tasks }.same_elements [[:clone, "http://localhost/test"]]
       asserts("update") { topic.parse(["--update"]).tasks }.same_elements [[:update, Dir.pwd]]
       asserts("repair") { topic.parse(["--repair"]).tasks }.same_elements [[:repair, Dir.pwd]]
       asserts("commit") { topic.parse(["--commit"]).tasks }.same_elements [[:commit, Dir.pwd]]
@@ -30,6 +30,21 @@ context "Options" do
 
       asserts("returns an OpenStruct") { topic.parse.is_a?(OpenStruct) }
       asserts("tasks is Array") { topic.parse.tasks.is_a?(Array) }
+
+      asserts("verbose") { topic.parse(["-v"]).verbose }.equals true
+      asserts("no-verbose") { topic.parse(["--no-verbose"]).verbose }.equals false
+
+      # TODO: How to test the exit properly?
+      asserts("version") { mock(SixSync::OptionsNetwork).exit { 0 }; topic.parse(["--version"])} #.equals 0
+      asserts("help") { mock(SixSync::OptionsNetwork).exit { 0 }; topic.parse(["--help"]) } #.equals 0
+
+      asserts("init") { topic.parse(["--init"]).tasks }.same_elements [[:init, Dir.pwd]]
+      asserts("init at specified dir") { topic.parse(["--init", "C:/test/test"]).tasks }.same_elements [[:init, "C:/test/test"]]
+      asserts("clone") { topic.parse(["--clone", "http://localhost/test"]).tasks }.same_elements [[:clone, "http://localhost/test"]]
+      asserts("update") { topic.parse(["--update"]).tasks }.same_elements [[:update, Dir.pwd]]
+      asserts("repair") { topic.parse(["--repair"]).tasks }.same_elements [[:repair, Dir.pwd]]
+      asserts("commit") { topic.parse(["--commit"]).tasks }.same_elements [[:commit, Dir.pwd]]
+      asserts("push") { topic.parse(["--push"]).tasks }.same_elements [[:push, Dir.pwd]]
     end
   end
 end
