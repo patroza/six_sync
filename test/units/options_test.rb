3551249f27c9ab382a892df8faf59c41628a711a
Sickboy <sb@dev-heaven.net>
2012-03-04 12:22:57 +0100

    Improved options test

--- test/units/options_test.rb
+++ test/units/options_test.rb
@@ -6,45 +6,55 @@ context "Options" do
     context "Main" do
       setup { SixSync::Options }
 
-      asserts("returns an OpenStruct") { topic.parse.is_a?(OpenStruct) }
-      asserts("tasks is Array") { topic.parse.tasks.is_a?(Array) }
+      context "Default params" do
+        setup { topic.parse }
+        asserts("returns an OpenStruct") { topic.is_a?(OpenStruct) }
+        asserts("tasks is Array") { topic.tasks.is_a?(Array) }
+      end
 
-      asserts("verbose") { topic.parse(["-v"]).verbose }.equals true
-      asserts("no-verbose") { topic.parse(["--no-verbose"]).verbose }.equals false
+      context "Specific params" do
+        asserts("verbose") { topic.parse(["-v"]).verbose }.equals true
+        asserts("no-verbose") { topic.parse(["--no-verbose"]).verbose }.equals false
 
-      # TODO: How to test the exit properly?
-      asserts("version") { mock(SixSync::Options).exit { 0 }; topic.parse(["--version"])} #.equals 0
-      asserts("help") { mock(SixSync::Options).exit { 0 }; topic.parse(["--help"]) } #.equals 0
+        # TODO: How to test the exit properly?
+        asserts("version") { mock(SixSync::Options).exit { 0 }; topic.parse(["--version"])} #.equals 0
+        asserts("help") { mock(SixSync::Options).exit { 0 }; topic.parse(["--help"]) } #.equals 0
 
-      asserts("init") { topic.parse(["--init"]).tasks }.same_elements [[:init, Dir.pwd]]
-      asserts("init at specified dir") { topic.parse(["--init", "C:/test/test"]).tasks }.same_elements [[:init, "C:/test/test"]]
-      asserts("clone") { topic.parse(["--clone", "http://localhost/test"]).tasks }.same_elements [[:clone, "http://localhost/test"]]
-      asserts("update") { topic.parse(["--update"]).tasks }.same_elements [[:update, Dir.pwd]]
-      asserts("repair") { topic.parse(["--repair"]).tasks }.same_elements [[:repair, Dir.pwd]]
-      asserts("commit") { topic.parse(["--commit"]).tasks }.same_elements [[:commit, Dir.pwd]]
-      asserts("push") { topic.parse(["--push"]).tasks }.same_elements [[:push, Dir.pwd]]
+        asserts("init") { topic.parse(["--init"]).tasks }.same_elements [[:init, Dir.pwd]]
+        asserts("init at specified dir") { topic.parse(["--init", "C:/test/test"]).tasks }.same_elements [[:init, "C:/test/test"]]
+        asserts("clone") { topic.parse(["--clone", "http://localhost/test"]).tasks }.same_elements [[:clone, "http://localhost/test"]]
+        asserts("update") { topic.parse(["--update"]).tasks }.same_elements [[:update, Dir.pwd]]
+        asserts("repair") { topic.parse(["--repair"]).tasks }.same_elements [[:repair, Dir.pwd]]
+        asserts("commit") { topic.parse(["--commit"]).tasks }.same_elements [[:commit, Dir.pwd]]
+        asserts("push") { topic.parse(["--push"]).tasks }.same_elements [[:push, Dir.pwd]]
+      end
     end
 
     context "Network" do
       setup { SixSync::NetworkOptions }
 
-      asserts("returns an OpenStruct") { topic.parse.is_a?(OpenStruct) }
-      asserts("tasks is Array") { topic.parse.tasks.is_a?(Array) }
+      context "Default params" do
+        setup { topic.parse }
+        asserts("returns an OpenStruct") { topic.is_a?(OpenStruct) }
+        asserts("tasks is Array") { topic.tasks.is_a?(Array) }
+      end
 
-      asserts("verbose") { topic.parse(["-v"]).verbose }.equals true
-      asserts("no-verbose") { topic.parse(["--no-verbose"]).verbose }.equals false
+      context "Specific params" do
+        asserts("verbose") { topic.parse(["-v"]).verbose }.equals true
+        asserts("no-verbose") { topic.parse(["--no-verbose"]).verbose }.equals false
 
-      # TODO: How to test the exit properly?
-      asserts("version") { mock(SixSync::NetworkOptions).exit { 0 }; topic.parse(["--version"])} #.equals 0
-      asserts("help") { mock(SixSync::NetworkOptions).exit { 0 }; topic.parse(["--help"]) } #.equals 0
+        # TODO: How to test the exit properly?
+        asserts("version") { mock(SixSync::NetworkOptions).exit { 0 }; topic.parse(["--version"])} #.equals 0
+        asserts("help") { mock(SixSync::NetworkOptions).exit { 0 }; topic.parse(["--help"]) } #.equals 0
 
-      asserts("init") { topic.parse(["--init"]).tasks }.same_elements [[:init, Dir.pwd]]
-      asserts("init at specified dir") { topic.parse(["--init", "C:/test/test"]).tasks }.same_elements [[:init, "C:/test/test"]]
-      asserts("clone") { topic.parse(["--clone", "http://localhost/test"]).tasks }.same_elements [[:clone, "http://localhost/test"]]
-      asserts("update") { topic.parse(["--update"]).tasks }.same_elements [[:update, Dir.pwd]]
-      asserts("repair") { topic.parse(["--repair"]).tasks }.same_elements [[:repair, Dir.pwd]]
-      asserts("commit") { topic.parse(["--commit"]).tasks }.same_elements [[:commit, Dir.pwd]]
-      asserts("push") { topic.parse(["--push"]).tasks }.same_elements [[:push, Dir.pwd]]
+        asserts("init") { topic.parse(["--init"]).tasks }.same_elements [[:init, Dir.pwd]]
+        asserts("init at specified dir") { topic.parse(["--init", "C:/test/test"]).tasks }.same_elements [[:init, "C:/test/test"]]
+        asserts("clone") { topic.parse(["--clone", "http://localhost/test"]).tasks }.same_elements [[:clone, "http://localhost/test"]]
+        asserts("update") { topic.parse(["--update"]).tasks }.same_elements [[:update, Dir.pwd]]
+        asserts("repair") { topic.parse(["--repair"]).tasks }.same_elements [[:repair, Dir.pwd]]
+        asserts("commit") { topic.parse(["--commit"]).tasks }.same_elements [[:commit, Dir.pwd]]
+        asserts("push") { topic.parse(["--push"]).tasks }.same_elements [[:push, Dir.pwd]]
+      end
     end
   end
 end
