c578f0022afdc9101f5d029ee861f3bc8033791e
Sickboy <sb@dev-heaven.net>
2012-03-09 19:52:50 +0100

    Fixed Version class

--- lib/six_sync/version.rb
+++ lib/six_sync/version.rb
@@ -1,15 +1,16 @@
 module SixSync
   # Version of the library
   VERSION = "0.0.1"
+  class Version
+    # Archived variants of files, for this version
+    attr_reader :pack_files
 
-  # Archived variants of files, for this version
-  attr_reader :pack_files
+    # Archived variants of files, for this version
+    attr_reader :wd_files
 
-  # Archived variants of files, for this version
-  attr_reader :wd_files
+    # Initializes the object
+    def initializes
 
-  # Initializes the object
-  def initializes
-
+    end
   end
 end
