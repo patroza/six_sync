96a9363dcec226f34ee8f066e0c2ff770c77eb54
Sickboy <sb@dev-heaven.net>
2012-03-09 19:19:30 +0100

    Added Version wd_files and pack_files

--- lib/six_sync/version.rb
+++ lib/six_sync/version.rb
@@ -1,4 +1,15 @@
 module SixSync
   # Version of the library
   VERSION = "0.0.1"
+
+  # Archived variants of files, for this version
+  attr_reader :pack_files
+
+  # Archived variants of files, for this version
+  attr_reader :wd_files
+
+  # Initializes the object
+  def initializes
+
+  end
 end
